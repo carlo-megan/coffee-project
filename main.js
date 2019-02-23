e9ca4657ca99db8dd1c7d4f979c9f471f0e8b6d5
Carlo Munguia <carlomunguia@carlos-air.home>
2019-02-23 15:19:03 -0600

    final version at this point lol

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 "use strict"
 
+// MAIN DISPLAY OF HTML VIA FUNCTION
 function renderCoffee(coffee) {
     var html = '<div class="coffee d-flex">';
-    html += '<h2 class="col-md card card-body" id="java-varietal">' + "java varietal:"+ '</h2>';
-    html += '<h2 class="col-md card card-body ml-2">' + coffee.name + '</h2>';
-    html += '<h2 class="col-md card card-body ml-2" id="roast-process">' + "roast process:"+ '</h2>';
-    html += '<p class="col-md card card-body ml-2">' + coffee.roast + '</p>';
+    html += '<h2 class="col-md card card-body bg-secondary" id="java-varietal">' + "java varietal:" + '</h2>';
+    html += '<h2 class="col-md card card-body ml-2 bg-secondary">' + coffee.name + '</h2>';
+    html += '<h2 class="col-md card card-body ml-2 bg-secondary" id="roast-process">' + "roast process:" + '</h2>';
+    html += '<p class="col-md card card-body ml-2 bg-secondary">' + coffee.roast + '</p>';
     html += '</div>';
 
     return html;
 }
 
+//THE POSITIVE ITERATING REFACTOR OF THE COFFEE ARRAY
 function renderCoffees(coffees) {
     var html = '';
     for (var i = 0; i < coffees.length; i++) {
@@ -19,12 +21,13 @@ function renderCoffees(coffees) {
     return html;
 }
 
+//THE ROAST SELECTION FUNCTION TO ACCURATELY GATHER GROUPING OF ID'S
 function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
     var selectedRoast = roastSelection.value;
     var filteredCoffees = [];
 
-    coffees.forEach(function(coffee) {
+    coffees.forEach(function (coffee) {
         if (coffee.roast === selectedRoast || selectedRoast === "all") {
             filteredCoffees.push(coffee);
         }
@@ -32,31 +35,24 @@ function updateCoffees(e) {
     tbody.innerHTML = renderCoffees(filteredCoffees);
 }
 
+//TYPEAHEAD STYLE REFACTOR FOR AUTOMATIC DISPLAY/SEARCH QUERY FUNCTIONALITY
+
 var coffeeSearch = document.getElementById("coffeeSearch");
 
-var searchQuery = function(e){
+var searchQuery = function (e) {
     var html = "";
-    for (var i = 0; i < coffees.length; i++ ){
-
-        if (coffees[i].name.toLowerCase().includes(coffeeSearch.value.toLowerCase())){
+    for (var i = 0; i < coffees.length; i++) {
+        if (coffees[i].name.toLowerCase().includes(coffeeSearch.value.toLowerCase())) {
             html = html + renderCoffee(coffees[i]);
-
         }
         tbody.innerHTML = html;
-
     }
-
 };
 
+// SPECIAL KEYUP LISTENER
+coffeeSearch.addEventListener("keyup", searchQuery);
 
-coffeeSearch.addEventListener("keyup",searchQuery);
-
-
-
-
-// var coffeeSearch = document.getElementById('coffees');
-// var coffeeTag = document.getElementsByTagName('name');
-
+// THIS IS THE COFFEE ARRAY/INFORMATION DATABASE.
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
     {id: 1, name: 'Light City', roast: 'light     '},
@@ -75,8 +71,7 @@ var coffees = [
     {id: 14, name: 'French', roast: 'dark'},
 ];
 
-
-
+// THESE ARE THE (MAIN) EVENT LISTENERS OF THE PROGRAM
 var tbody = document.querySelector('#coffees');
 var submitButton = document.querySelector('#submit');
 var roastSelection = document.querySelector('#roast-selection');
